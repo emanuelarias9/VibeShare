fceb966310e1d3bf2e979b69553b6ff8e3a0fc85
Emanuel Arias <Emanuelarias9@gmail.com>
2025-08-03 20:46:33 -0500

    Refactor project structure: remove unused CSS, update App component, and add responsive styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./assets/css/normalize.css";
+import "./assets/css/styles.css";
+import "./assets/css/responsive.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);

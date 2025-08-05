6c33b1cd32d95b8044eab17a0ef587d0f9fe1c58
Emanuel Arias <Emanuelarias9@gmail.com>
2025-08-04 23:33:30 -0500

    Add Header and Nav components; update App to use Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-// import { useState } from "react";
+import { Header } from "./components/layout/general/Header";
 
 function App() {
   return (
     <div className="layout">
-      <h1>VibeShare</h1>
+      <Header />
     </div>
   );
 }

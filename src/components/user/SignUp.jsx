544855007da97c53d4e2f9364b8c784da9a08fd7
Emanuel Arias <Emanuelarias9@gmail.com>
2025-08-14 21:24:30 -0500

    Add useForm hook and integrate form handling in SignUp component

--- src/components/user/SignUp.jsx
+++ src/components/user/SignUp.jsx
@@ -1,32 +1,39 @@
 import React from "react";
+import { useForm } from "../../hooks/useForm";
 
 export const SignUp = () => {
+  const { form, changed } = useForm({});
+  const saveUser = (e) => {
+    e.preventDefault();
+    let newUser = form;
+    console.log(newUser);
+  };
   return (
     <>
       <header className="content__header content__header--public">
         <h1 className="content__title">Registro</h1>
       </header>
       <div className="content__posts">
-        <form className="register-form">
+        <form className="register-form" onSubmit={saveUser}>
           <div className="form-group">
             <label htmlFor="username">Nombre de usuario</label>
-            <input type="text" name="username" />
+            <input type="text" name="username" onChange={changed} />
           </div>
           <div className="form-group">
             <label htmlFor="nick">Nick</label>
-            <input type="text" name="nick" />
+            <input type="text" name="nick" onChange={changed} />
           </div>
           <div className="form-group">
             <label htmlFor="email">Correo</label>
-            <input type="email" name="email" />
+            <input type="email" name="email" onChange={changed} />
           </div>
           <div className="form-group">
             <label htmlFor="email">Correo</label>
-            <input type="email" name="email" />
+            <input type="email" name="email" onChange={changed} />
           </div>
           <div className="form-group">
             <label htmlFor="password">Contraseña</label>
-            <input type="password" name="password" />
+            <input type="password" name="password" onChange={changed} />
           </div>
           <input type="submit" value="Registrate" className="btn btn-success" />
         </form>

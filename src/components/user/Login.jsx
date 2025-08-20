6013b3aded6c1c09c8e5e54bbcc2e16177dfaa49
Emanuel Arias <Emanuelarias9@gmail.com>
2025-08-19 22:09:51 -0500

    Implement Login form structure with email and password fields

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -6,7 +6,23 @@ export const Login = () => {
       <header className="content__header content__header--public">
         <h1 className="content__title">Iniciar sesión</h1>
       </header>
-      <div className="content__posts"></div>
+      <div className="content__posts">
+        <form className="form-login">
+          <div className="form-group">
+            <label htmlFor="Email">Email</label>
+            <input type="email" name="email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" name="password" />
+          </div>
+          <input
+            type="submit"
+            value="Iniciar sesion"
+            className="btn btn-success"
+          />
+        </form>
+      </div>
     </>
   );
 };
